6c4eae5c0fb6eed0d190775409012aa7cd214f96
Luke Seemann <bikesarefun@gmail.com>
2021-03-01 09:05:08 -0600

    Add rules to score

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -6,15 +6,17 @@ import {
   createStyles,
   Box,
   Typography,
+  Divider,
 } from '@material-ui/core';
 
 const styles = (theme: Theme) => createStyles({
   root: {
+    alignItems: 'center',
     alignSelf: 'center',
     backgroundColor: theme.palette.primary.dark,
     borderRadius: theme.shape.borderRadius,
     color: theme.palette.common.white,
-    columnGap: '0.5rem',
+    columnGap: '0.25rem',
     display: 'flex',
     justifyContent: 'center',
     margin: '1rem ',
@@ -25,14 +27,31 @@ const styles = (theme: Theme) => createStyles({
     },
     [theme.breakpoints.up('md')]: {
       /* tablet landscape */
-      columnGap: '1rem',
+      columnGap: '0.5rem',
     },
     [theme.breakpoints.up('lg')]: {
-      columnGap: '2rem',
+      columnGap: '1rem',
       /* desktop */
     },
   },
+  divider: {
+    backgroundColor: theme.palette.common.white,
+    display: 'flex',
+    opacity: '0.5',
+    width: 1,
+    height: '1.25rem',
+    [theme.breakpoints.up('sm')]: {
+      height: '1.5rem',
+    },
+  },
+  dividerLarge: {
+    height: '2.5rem',
+    [theme.breakpoints.up('sm')]: {
+      height: '3rem',
+    },
+  },
   stat: {
+    alignSelf: 'flex-start',
     flexBasis: 0,
     flexGrow: 1,
     textAlign: 'center',
@@ -79,14 +98,21 @@ const Stats: React.FC<Props> = ({ stats }: Props) => {
         <Typography className={classes.score}>{correct}</Typography>
         <Typography className={classes.label}>Correct</Typography>
       </Box>
+      <Divider className={classes.divider} />
       <Box className={classes.stat}>
         <Typography className={classes.score}>{outOf - correct}</Typography>
         <Typography className={classes.label}>Incorrect</Typography>
       </Box>
+      <Divider className={`
+        ${classes.divider}
+        ${classes.dividerLarge}
+        `}
+      />
       <Box className={classes.stat}>
         <Typography className={classes.score}>{currentStreak}</Typography>
         <Typography className={classes.label}>Current streak</Typography>
       </Box>
+      <Divider className={classes.divider} />
       <Box className={classes.stat}>
         <Typography className={classes.score}>{longestStreak}</Typography>
         <Typography className={classes.label}>Longest streak</Typography>
